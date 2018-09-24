be2f60aa81572959df9d4558fbf98418a5b5e67a
Andreas Schmitz <schmitz@terrestris.de>
2018-09-24 14:42:21 +0200

    add webpack build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ChartDataUtil from './ChartDataUtil/ChartDataUtil';
 import ChartRenderer from './ChartRenderer/ChartRenderer';
 import ScaleUtil from './ScaleUtil/ScaleUtil';
-import TimeseriesComponent from './TimeseriesComponent/TimeseriesComponents';
+import TimeseriesComponent from './TimeseriesComponent/TimeseriesComponent';
 
 export {
   ChartDataUtil,
